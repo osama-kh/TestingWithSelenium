e21f2276163440ad60772c258797606296f95677
osamakh <osamakh.tj@gmail.com>
2023-08-31 17:37:20 +0300

    set tests

--- src/test/java/UI/RestaurantTest.java
+++ src/test/java/UI/RestaurantTest.java
@@ -1,11 +1,39 @@
 package UI;
 
-import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RestaurantTest {
 
 
+    @Before
+    public void run_driver(){
 
+
+
+    }
+
+
+    @Test
+    public void add_new_restaurant_TEST(){
+
+
+
+
+    }
+
+    @After
+    public void finishing(){
+
+
+
+
+    }
 
 
 
